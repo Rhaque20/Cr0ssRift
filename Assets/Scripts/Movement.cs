06a4f99a1abd8a57f7801e1982a3c27ab025d537
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2024-05-16 17:37:11 -0400

    Implemented enhanced input and basic movement.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    protected float _moveSpeed = 10f;
+    protected Animator _anim;
+    protected Rigidbody _rigid;
+
+    protected Vector3 _direction = Vector3.zero;
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        _anim = transform.GetChild(0).GetComponent<Animator>();
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void Move()
+    {
+
+    }
+
+    protected virtual void FaceDirection(float x)
+    {
+        transform.localScale = new Vector3(Mathf.Sign(x),transform.localScale.y,transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : Movement
+{
+    PlayerInput _playerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        _playerInput = GetComponent<PlayerInput>();
+        PlayerControls playerInputActions = new PlayerControls();
+        playerInputActions.Combat.Enable();
+        playerInputActions.Combat.Movement.performed += GetDirectionFromInput;
+        playerInputActions.Combat.Movement.canceled += ctx => _direction = Vector3.zero;
+        
+    }
+
+    void GetDirectionFromInput(InputAction.CallbackContext ctx)
+    {
+        Vector2 inputVector = ctx.ReadValue<Vector2>();
+
+        _direction = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
+    protected override void Move()
+    {
+        if (_direction.x != 0)
+            FaceDirection(_direction.x);
+        _rigid.MovePosition(transform.position + (_direction.normalized * Time.deltaTime * _moveSpeed));
+        _anim.SetFloat("MoveIntensity",_direction.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+}
